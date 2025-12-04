48cc06dc2187813b9f308309cb06bf1fa3ec71c6
João Victor Cruz <joao.cruz.jc@ufpi.edu.br>
2025-12-03 22:02:33 -0300

    fix: caminhos do pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import "./styles/global.css";
 
-const basename =
-  import.meta.env.MODE === "production" ? "/inct-site" : "/";
+const isProd = import.meta.env.MODE === "production";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
+    {isProd ? (
+      <HashRouter>
+        <App />
+      </HashRouter>
+    ) : (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )}
   </React.StrictMode>
 );
